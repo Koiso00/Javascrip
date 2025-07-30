70ea2776ef952c1507d5318b2c26c71a0d29f720
LE VAN PHONG <levanphong1910@gmail.com>
2025-07-30 23:39:51 +0700

    video 18

---
+++ video18/video18.js
@@ -0,0 +1,20 @@
+console.log ("video18");
+//number 
+const myAge =25 ;
+const myScore =9.1;
+//string 
+const name = "hoidanit";
+//boolean : true or false 
+const nam = true ;
+console.log (myAge)
+console.log (myScore) 
+console.log (name)
+console.log (nam)
+//undefined chua duoc dinh nghia 
+let bla ;
+console.log (bla)
+//null 
+let testnull= null;
+console.log (testnull);
+//datatyple 
+console .log (typeof(23));
