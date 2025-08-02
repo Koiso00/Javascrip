abf1be8cb35bd01aa6a410dc7c258026401d3c8a
LE VAN PHONG <levanphong1910@gmail.com>
2025-08-02 16:12:25 +0700

    video 21

---
+++ video21/video21.js
@@ -0,0 +1,10 @@
+console.log ("video 21")
+//comment ctr+/  ,  /*  aaa  */
+console.log("info")
+console.error("oops my mistake")
+console.warn("test warning")
+let name = "hoidanit";
+let age = 20;
+//console.log ("name ",name,", age ",age)
+console.log (name,age)
+console.log ("%cCANH BAO","color:red;font-weight:bold;")
