61ae2f8791acaa6ea637f371fe42b490b8f6b367
LE VAN PHONG <levanphong1910@gmail.com>
2025-08-02 15:20:05 +0700

    video 19

---
+++ video19/video19.js
@@ -0,0 +1,11 @@
+console.log ('video19')
+//object : key-value
+const persont ={
+name : "hoidanit",
+age : 25,
+address : "hanoi"
+}
+console .log (persont)
+//array
+const scores =[10,3,4,8,9]
+console .log (scores)
