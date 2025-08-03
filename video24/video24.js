9ecadc07761a025244c26a917d83b577cb7d6096
LE VAN PHONG <levanphong1910@gmail.com>
2025-08-03 17:48:03 +0700

    video 24

---
+++ video24/video24.js
@@ -0,0 +1,15 @@
+console.log ("video 24")
+const age = 25 ;
+if (age > 18){
+    console.log ("enjoy your video")
+}else {
+    console.log ("you can't watch video")
+}
+const score =10;
+if (score >9) {
+    console.log ("Giỏi")
+}else if (score <=9 && score >=6){
+    console.log ("Khá")
+}else {
+    console.log ("Trung Binh")
+}
