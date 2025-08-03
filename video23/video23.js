b9a7e3aa18ba072d2e4a418cd6db158140b2a29a
LE VAN PHONG <levanphong1910@gmail.com>
2025-08-03 17:30:58 +0700

    video 23

---
+++ video23/video23.js
@@ -0,0 +1,10 @@
+const fullname  = "le van phong";
+const today = new Date();
+const currenyear = today.getFullYear();
+const namsinh=2006;
+const age = currenyear- namsinh;
+const sinhvien = true;
+console.log (`
+ten: ${fullname}
+tuoi: ${age}
+sinh vien: ${sinhvien}`)
