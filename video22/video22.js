c70157b4f9c6faf7fd9cc1ba35358da658ce0c6c
LE VAN PHONG <levanphong1910@gmail.com>
2025-08-03 16:02:17 +0700

    video 22

---
+++ video22/video22.js
@@ -0,0 +1,7 @@
+console.log ("video22")
+const name ="hoidanit";
+const age = 25 ;
+const introduction = "name "+ name +" age "+age;
+console.log (introduction)
+const intro = `my name is ${name} and  i'm ${age}`;
+console.log (intro)
