ad16b3da32dc2431f51fcb0e4b125ec30febbc7e
LE VAN PHONG <levanphong1910@gmail.com>
2025-08-05 00:07:31 +0700

    video 25

---
+++ video25/video27.js
@@ -0,0 +1,14 @@
+console.log ("video 27")
+const score =8;
+switch (true) {
+    case (score >=8 && score <=10) :
+        console.log("gioi")
+        break;
+    case (score>=6&& score<8) :
+        console.log("kha")
+    case (score>=4&& score <6):
+        console.log ("trung binh")
+    default:
+        console.log ("yeu")
+        break;
+}
