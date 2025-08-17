760b4378d3ca89321cc8fbaf5fc3fd4342ae5f9e
LE VAN PHONG <levanphong1910@gmail.com>
2025-08-17 22:20:03 +0700

    video 28

---
+++ video28/video30.js
@@ -0,0 +1,13 @@
+console.log ("video 30")
+function greeting (){
+    console.log ("say hi")
+}
+greeting ();
+
+// sum a+b
+function sum (a,b){
+    return a+b;
+}
+
+console.log (sum(6,9))
+console.log (sum ("hoidanit ","with eric"))
