a07b3639c9723f3e77e14ee75d8a02395fb118bb
LE VAN PHONG <levanphong1910@gmail.com>
2025-08-17 22:18:20 +0700

    video 26

---
+++ video26/video28.js
@@ -0,0 +1,10 @@
+console.log ("video 28")
+for (let i=0 ;i<=5;i++){
+    console.log (`i = ${i}`)
+}
+console.log ("-------------")
+let a=0;
+while (a<=5){
+    console.log (`i = ${a}`)
+    a++;
+}
