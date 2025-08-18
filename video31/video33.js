81b97dd079b05d52df76e743985d56a8e6dbfd23
LE VAN PHONG <levanphong1910@gmail.com>
2025-08-18 09:46:46 +0700

    video 31

---
+++ video31/video33.js
@@ -0,0 +1,14 @@
+console.log ("video 33")
+let b=10;
+function name (a,b){
+    if (typeof a === 'number'){
+        let s=10;
+        return s;
+       
+    }else{
+        let s=5;
+        return s;
+    }
+
+}
+console.log (name ('a',6))
