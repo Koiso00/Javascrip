bc1a9fd2c3c722ec38233bea32da1d596bd1bc39
LE VAN PHONG <levanphong1910@gmail.com>
2025-08-18 16:58:04 +0700

    video 34

---
+++ video34/video38.js
@@ -0,0 +1,10 @@
+console.log ("video 38")
+const name= ["eric", "hoidanit","vietnam","thongnhat"]
+name [0]="datnuoc";
+console.log (`before:`,name)
+name.push("true",[1,2,3])
+console.log ("after:",name)
+name.unshift(null)
+console.log (name)
+name.shift()
+console.log (name)
