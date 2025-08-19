092b2e1d3d692d3d6207ba860be55bb8130fcf79
LE VAN PHONG <levanphong1910@gmail.com>
2025-08-19 17:10:32 +0700

    video 42 object

---
+++ video38/video42.js
@@ -0,0 +1,23 @@
+console.log ("video 42")
+const scores =[14,14,15,3,1]
+
+//key :value
+const sv1 = {
+    score :10,
+    name: "hoidanit",
+    address: {
+        city :"hanoi",
+        country:"vietnam"
+    }
+}
+
+const sv2 = {
+    score :4,
+    name: "eric",
+    address: {
+        city :"hanoi",
+        country:"vietnam"
+    }
+}
+const sinhvien =[sv1,sv2]
+console.log (sinhvien)
