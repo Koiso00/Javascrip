222575de9d3b5f9437818049ba05c39a497b429a
LE VAN PHONG <levanphong1910@gmail.com>
2025-08-20 15:50:22 +0700

    video 40

---
+++ video40/video44.js
@@ -0,0 +1,29 @@
+console.log ("video 44")
+const sv1 ={
+    username:"hoidanit",
+    score:9.3
+}
+const sv2 ={
+    username:"eric",
+    score:3
+}
+const sv3 ={
+    username:"bla bla",
+    score:5
+}
+const sinhvien =[sv1,sv2,sv3]
+
+// console.log (sinhvien)
+// sinhvien.forEach ((value,index)=>{
+//     console.log("index = ",index, "name =",value.username)
+// })
+
+const person ={
+    email: "hoidanit@gmail.com",
+    age :25,
+    address :"vietnam"
+}
+for (let key in person){
+    console.log (key,person[key]);
+}
+for ()
