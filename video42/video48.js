4f427afb71e802e5277ae750dd9856a0c279de68
LE VAN PHONG <levanphong1910@gmail.com>
2025-08-22 15:41:09 +0700

    video 42

---
+++ video42/video48.js
@@ -0,0 +1,7 @@
+console.log ("video 48")
+
+const element =document.getElementById ("username")
+const myDiv = document.querySelector(".hoidanit")
+const allMyDiv = document.querySelectorAll(".hoidanit")
+
+console.log (allMyDiv)
