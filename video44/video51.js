96f849e7e94fd834520c6cb169ae04b7ac438bc6
LE VAN PHONG <levanphong1910@gmail.com>
2025-08-22 16:34:27 +0700

    video 44

---
+++ video44/video51.js
@@ -0,0 +1,11 @@
+console.log ("video 51")
+const element= document.getElementById("click")
+
+const handleClickBtn = ()=>{
+    console.log ("hello tom")
+}
+element.addEventListener("click",handleClickBtn)
+
+// element.addEventListener("click",function (){
+//     console.log ("hello world")
+// })
