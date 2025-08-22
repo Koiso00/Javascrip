02a2d9dc8f94a3a1e5e28f935ae55038435adf8e
LE VAN PHONG <levanphong1910@gmail.com>
2025-08-22 16:21:31 +0700

    video 43 button

---
+++ video43/video50.js
@@ -0,0 +1,9 @@
+console.log ("video 50")
+// const element = document.getElementById("myButton")
+// console.log (element)
+const whenButton=()=>{
+    console.log ("hello world")
+}
+function whenButton1 (){
+    console.log ("hello world ")
+}
